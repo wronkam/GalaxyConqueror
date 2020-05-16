86ea1f9f20187a6ec18c6c977c024276c722d9ad
snkyy <xmatiforfun@gmail.com>
2020-05-16 18:53:20 +0200

    Enemies are rotated correctly!

--- galaxyconqueror/src/main/java/GalaxyConqueror/View/View.java
+++ galaxyconqueror/src/main/java/GalaxyConqueror/View/View.java
@@ -9,6 +9,6 @@ public class View {
     public static Image background = new Image("file:resources/background.jpg", SCREEN_WIDTH, SCREEN_HEIGHT, false, true);
     public static Image bullet = new Image("file:resources/bullet.png",BULLET_WIDTH,BULLET_HEIGHT,true,true);
     public static Image greenbullet = new Image("file:resources/greenbullet.png", BULLET_WIDTH, BULLET_HEIGHT, true, true);
-    public static Image enemy = new Image("file:resources/enemy.png", ENEMY_WIDTH, ENEMY_HEIGHT, true, true);
+    public static Image enemy = new Image("file:resources/enemy.png", ENEMY_HEIGHT, ENEMY_WIDTH, true, true);
     public static Image menubg = new Image("file:resources/menubackground.jpg", SCREEN_WIDTH, SCREEN_HEIGHT, true, true);
 }
